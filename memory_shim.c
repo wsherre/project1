665d9b0462e48d717e1a6948cb3321fd64d2e583
Will Sherrer <wsherre@clemson.edu>
2021-01-27 15:56:24 -0500

    test

--- memory_shim.c
+++ memory_shim.c
@@ -11,6 +11,8 @@ void add_node(int, void*);
 void* (*real_malloc)(size_t)=NULL;
 void (*real_free)(void*)=NULL;
 
+//each malloc call will be held as a struct with a memory address and byte number
+//it'll be stored in a link list
 typedef struct list{
     int data;
     void* address;
@@ -21,6 +23,7 @@ list *head = NULL;
 int check_for_leak = 0;
 
 void lib_init(){
+    //ensures the list will only run after the library call
     check_for_leak = 1;
 }
 
@@ -29,10 +32,11 @@ void lib_destroy(){
         int total = 0, total_bytes = 0, current_bytes = 0;
         list* temp = head;
         list* kill = NULL;
+        //loops through the remaining list items and prints them out as a leak
         while(temp != NULL){
             total++;
             total_bytes += temp->data;
-        current_bytes = temp->data;
+            current_bytes = temp->data;
             kill = temp;
             temp = temp->next;
             real_free(kill);
@@ -41,6 +45,7 @@ void lib_destroy(){
         fprintf(stderr, "TOTAL\t%d\t%d\n", total, total_bytes);
 }
 
+//remove the node from the linked list and free the data from real memory
 void free(void* ptr){
     
     if(real_free == NULL){
@@ -50,6 +55,7 @@ void free(void* ptr){
     real_free(ptr);
 }
 
+//assign real memory and add the node into the linked list
 void *malloc(size_t size)
 {
     if(real_malloc == NULL){
@@ -62,26 +68,31 @@ void *malloc(size_t size)
     return p;
 }
 
-
+//add a new node at the end of the linked list
 void add_node(int byte, void* ptr){
     list *new_node = real_malloc(sizeof(list));
     new_node->data = byte;
     new_node->address = ptr;
     list* prev = head;
     list* current = head;
+
     if(head == NULL){
         head = new_node;
     }else{
         current = current->next;
+
         while(current != NULL){
             prev = current;
             current = current->next;
         }
+        
         prev->next = new_node;
         new_node->next = NULL;
     }
 }
 
+//remove the node with the proper memory address 
+//then free that memory
 void remove_node(void* ptr){
     list* prev = head;
     list* current = head;
