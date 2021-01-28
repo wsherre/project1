65d10679811eec83ffedf205f45d9165a22bcc62
Will Sherrer <wsherre@clemson.edu>
2021-01-28 14:30:29 -0500

    yo

--- leakcount.c
+++ leakcount.c
@@ -8,21 +8,23 @@
 //first submission to see if i did it right please dont grade my lack of comments
 int main(int argc, char **argv){
 
-    char *envp[] = {"LD_PRELOAD=./memory_shim.so", NULL};
-    for(int i = 0; i < argc; ++i){
-        argv[i] = argv[i+1];
-    }
-    argv[argc] = NULL;
-    char c =  *argv[0];
-    pid_t pid;
-    if((pid = fork()) == 0){
-        if('.' != c){
-            execvp(argv[0], argv);
-        }else{
-            execvpe(argv[0], argv, envp);
+    if(argc > 1){
+        char *envp[] = {"LD_PRELOAD=./memory_shim.so", NULL};
+        for(int i = 0; i < argc; ++i){
+            argv[i] = argv[i+1];
         }
-    }else{
-        waitpid(pid, NULL, 0);
+        argv[argc] = NULL;
+        char c =  *argv[0];
+        pid_t pid;
+        if((pid = fork()) == 0){
+            if('.' != c){
+                execvp(argv[0], argv);
+            }else{
+                execvpe(argv[0], argv, envp);
+            }
+        }else{
+            waitpid(pid, NULL, 0);
+        }
     }
     return 0;
 }
--- sctracer.c
+++ sctracer.c
@@ -1,3 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+//#include <sys/ptrace.h>
+
+
 int main(int argc, char** argv){
 //placeholder file
 }
