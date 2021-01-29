c6208387ea0df94c56b2a3fe33d5390375a16e7a
Will Sherrer <wsherre@clemson.edu>
2021-01-29 14:48:26 -0500

    adsf

--- leakcount.c
+++ leakcount.c
@@ -19,7 +19,7 @@ int main(int argc, char **argv){
         if((pid = fork()) == 0){
             //execvpe(argv[1], (argv + 1),  envp);
             for(int i = 0; i < argc - 1; ++i){
-                printf("%s\n", *argv[i]);
+                printf("%s\n", argv[i]);
             }
 
             
