697a7e8f31cff870d76bad0068cd9a2a3ad82d5c
Will Sherrer <wsherre@clemson.edu>
2021-01-28 22:59:10 -0500

    p

--- sctracer.c
+++ sctracer.c
@@ -1,11 +1,15 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
 #include <sys/ptrace.h>
-#include <sys/stat.h>
-#include <errno.h>
 #include <sys/reg.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <time.h>
 
 #define max_array_size 320
 typedef struct scall{
