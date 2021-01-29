2bc18feb4ba821151c2a6f842a41ec0655262508
Will Sherrer <wsherre@clemson.edu>
2021-01-29 14:37:31 -0500

    adf

--- leakcount.c
+++ leakcount.c
@@ -13,6 +13,7 @@ int main(int argc, char **argv){
         FILE*in;
         in = fopen("in.txt", "w+");
         fprintf(in, "0 0");
+        fclose(in);
         
         pid_t pid;
         if((pid = fork()) == 0){
@@ -21,7 +22,7 @@ int main(int argc, char **argv){
         }else{
             waitpid(pid, NULL, 0);
             int total = 0, total_bytes = 0;
-            fscanf(in, "%d %d", &total, &total_bytes);
+            //fscanf(in, "%d %d", &total, &total_bytes);
             fprintf(stderr, "TOTAL\t%d\t%d\n", total, total_bytes);
         }
         //execlp("rm", "rm", "in.txt");
