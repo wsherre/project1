37e02a1b98b3d9a26ea3dffbf493ceba1b062196
Will Sherrer <wsherre@clemson.edu>
2021-02-01 18:03:59 -0500

    asdf

--- sctracer.c
+++ sctracer.c
@@ -51,45 +51,47 @@ int main(int argc, char** argv){
         int status,syscall_num;
         //I'm the parent...keep tabs on that child process
 
-        //wait for the child to stop itself
-        waitpid(child, &status, 0);
-
-        //this option makes it easier to distinguish normal traps from
-        //system calls
-        ptrace(PTRACE_SETOPTIONS, child, 0,
-                PTRACE_O_TRACESYSGOOD);
-
-        do{
-            //Request: I want to wait for a system call
-            ptrace(PTRACE_SYSCALL, child, 0, 0);
-
-            //actually wait for child status to change
+        while(!(WIFSTOPPED(status)){
+            //wait for the child to stop itself
             waitpid(child, &status, 0);
 
-            //there are differented reasons that a child's
-            //status might change. Check to see if the child
-            //exited
-            if (WIFEXITED(status)) {
-                //the child exited...let's exit too
-                exit(1);
-            }
-            
-            //wait until the process is stopped or bit 7 is set in
-            //the status (see man page comment on
-            //PTRACE_O_TRACESYSGOOD)
-        } while (!(WIFSTOPPED(status) && WSTOPSIG(status) & 0x80));
+            //this option makes it easier to distinguish normal traps from
+            //system calls
+            ptrace(PTRACE_SETOPTIONS, child, 0,
+                    PTRACE_O_TRACESYSGOOD);
 
-        //read out the saved value of the RAX register,
-        //which contains the system call number
-        //For 32-bit machines, you would use EAX.
-        syscall_num = ptrace(PTRACE_PEEKUSER, child, sizeof(long)*ORIG_RAX, NULL);
-        array[syscall_num]++;
-        
-        //for this example, I only want the first
-        //system call. So...
-        //let the child run to completion
-        ptrace(PTRACE_CONT, child, NULL, NULL);
-        waitpid(child, NULL, 0);
+            do{
+                //Request: I want to wait for a system call
+                ptrace(PTRACE_SYSCALL, child, 0, 0);
+
+                //actually wait for child status to change
+                waitpid(child, &status, 0);
+
+                //there are differented reasons that a child's
+                //status might change. Check to see if the child
+                //exited
+                if (WIFEXITED(status)) {
+                    //the child exited...let's exit too
+                    exit(1);
+                }
+                
+                //wait until the process is stopped or bit 7 is set in
+                //the status (see man page comment on
+                //PTRACE_O_TRACESYSGOOD)
+            } while (!(WIFSTOPPED(status) && WSTOPSIG(status) & 0x80));
+
+            //read out the saved value of the RAX register,
+            //which contains the system call number
+            //For 32-bit machines, you would use EAX.
+            syscall_num = ptrace(PTRACE_PEEKUSER, child, sizeof(long)*ORIG_RAX, NULL);
+            array[syscall_num]++;
+            
+            //for this example, I only want the first
+            //system call. So...
+            //let the child run to completion
+            //ptrace(PTRACE_CONT, child, NULL, NULL);
+            waitpid(child, NULL, 0);
+        }
     }
     print_array(array, argv);
     return 0;
